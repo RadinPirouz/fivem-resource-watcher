8697bdceb3200a8952aee52a20df05992d5f0cda
RadinPirouz <75082987+RadinPirouz@users.noreply.github.com>
2023-11-04 18:58:10 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-beginswith() { case $2 in "$1"*) true ;; *) false ;; esac }
+beginswith() {
+  case $2 in "$1"*) true ;; *) false ;; esac
+}
 
 exists_in_array() {
   local element="$1"
@@ -24,13 +26,13 @@ append_if_not_exists() {
 }
 
 icecon_command() {
-    icecon --command "$1" "${SERVER_IP}:${SERVER_PORT}" "${RCON_PASSWORD}"
+  icecon --command "$1" "${SERVER_IP}:${SERVER_PORT}" "${RCON_PASSWORD}"
 }
 
 get_player_count() {
-    response=$(curl -s "${SERVER_IP}:${SERVER_PORT}/players.json")
-    player_count=$(echo "$response" | jq 'length')
-    echo "$player_count"
+  response=$(curl -s "${SERVER_IP}:${SERVER_PORT}/players.json")
+  player_count=$(echo "$response" | jq 'length')
+  echo "$player_count"
 }
 
 RESTART_INDIVIDUAL_RESOURCES=$1
@@ -44,49 +46,46 @@ IGNORED_RESOURCES=$7
 git config --global --add safe.directory /github/workspace
 
 if [ ${GITHUB_BASE_REF} ]; then
-    # Pull Request
-    git fetch origin ${GITHUB_BASE_REF} --depth=1
-    export DIFF=$(git diff --name-only origin/${GITHUB_BASE_REF} ${GITHUB_SHA})
-    echo "Diff between origin/${GITHUB_BASE_REF} and ${GITHUB_SHA}"
+  # Pull Request
+  git fetch origin ${GITHUB_BASE_REF} --depth=1
+  export DIFF=$(git diff --name-only origin/${GITHUB_BASE_REF} ${GITHUB_SHA})
+  echo "Diff between origin/${GITHUB_BASE_REF} and ${GITHUB_SHA}"
 else
-    # Push
-    git fetch origin ${GITHUB_EVENT_BEFORE} --depth=1
-    export DIFF=$(git diff --name-status ${GITHUB_EVENT_BEFORE} ${GITHUB_SHA})
-    echo "Diff between ${GITHUB_EVENT_BEFORE} and ${GITHUB_SHA}"
+  # Push
+  git fetch origin ${GITHUB_EVENT_BEFORE} --depth=1
+  export DIFF=$(git diff --name-status ${GITHUB_EVENT_BEFORE} ${GITHUB_SHA})
+  echo "Diff between ${GITHUB_EVENT_BEFORE} and ${GITHUB_SHA}"
 fi
 
 resources_to_restart=
 
 IFS=$'\n'
 for changed in $DIFF; do
-    changed=${changed#??}
-    if beginswith "${RESOURCES_FOLDER}" "${changed}"; then
-        filtered=${changed##*]/} # Remove subfolders
-        filtered=${filtered%%/*} # Remove filename and get the folder which corresponds to the resource name
-        resources_to_restart="$(append_if_not_exists "$filtered" "$resources_to_restart")"
-    fi
+  changed=${changed#??}
+  if beginswith "${RESOURCES_FOLDER}" "${changed}"; then
+    filtered=${changed##*]/} # Remove subfolders
+    filtered=${filtered%%/*} # Remove filename and get the folder which corresponds to the resource name
+    resources_to_restart="$(append_if_not_exists "$filtered" "$resources_to_restart")"
+  fi
 done
 unset IFS
 
-if [ -z "$resources_to_restart" ]; then
-    echo "Nothing to restart"
-else
-    player_count=$(get_player_count)
-    if [ "$RESTART_SERVER_WHEN_0_PLAYERS" = true ] && [ "$player_count" -eq 0 ]; then
-        echo "Will restart the whole server due to 0 players"
-        icecon_command "quit"
-    elif [ "$RESTART_INDIVIDUAL_RESOURCES" = true ]; then
-        echo "Will restart individual resources"
-        for resource in $resources_to_restart; do
-            if exists_in_array "${resource}" "${IGNORED_RESOURCES}"; then
-                echo "Ignoring restart of the resource ${resource}"
-            else
-                echo "Restarting ${resource}"
-                icecon_command "ensure ${resource}"
-            fi
-        done
+player_count=$(get_player_count)
+
+if [ "$RESTART_SERVER_WHEN_0_PLAYERS" = true ] && [ "$player_count" -eq 0 ]; then
+  echo "Will restart the whole server due to 0 players"
+  icecon_command "quit"
+elif [ "$RESTART_INDIVIDUAL_RESOURCES" = true ]; then
+  echo "Will restart individual resources"
+  for resource in $resources_to_restart; do
+    if exists_in_array "${resource}" "${IGNORED_RESOURCES}"; then
+      echo "Ignoring restart of the resource ${resource}"
     else
-        echo "Will restart the whole server"
-        icecon_command "quit"
+      echo "Restarting ${resource}"
+      icecon_command "ensure ${resource}"
     fi
+  done
+else
+  echo "Will restart the whole server"
+  icecon_command "quit"
 fi
